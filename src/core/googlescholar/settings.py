5c67a612ca8ec90c3ffdc9722710480088bdb152
Min Zhao <sebzhaomin@gmail.com>
2017-03-29 22:13:37 +0200

    Use TOR and polipo SOCK

--- src/core/googlescholar/settings.py
+++ src/core/googlescholar/settings.py
@@ -15,10 +15,20 @@ SPIDER_MODULES = ['core.googlescholar.spiders']
 NEWSPIDER_MODULE = 'core.googlescholar.spiders'
 
 # Log config
-LOG_LEVEL = 'INFO'
-LOG_FORMAT = '\t-%(asctime)s [%(name)s] %(levelname)s: %(message)s'
+#LOG_LEVEL = 'INFO'
+#LOG_FORMAT = '\t-%(asctime)s [%(name)s] %(levelname)s: %(message)s'
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'googlescholar (+http://www.yourdomain.com)'
+
+# PROXY
+HTTP_PROXY = 'http://127.0.0.1:8123'
+
+# USER_AGENT
+USER_AGENT_LIST = [
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.36 Safari/535.7',
+    'Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:16.0) Gecko/16.0 Firefox/16.0',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/534.55.3 (KHTML, like Gecko) Version/5.1.3 Safari/534.53.10',
+    ]
+
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -55,8 +65,9 @@ COOKIES_ENABLED = False
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
-        'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware' : None,
-        'core.googlescholar.middlewares.RotateUserAgentMiddleware' :400
+        'core.googlescholar.middlewares.RandomUserAgentMiddleware' :400,
+        'core.googlescholar.middlewares.ProxyMiddleware': 410,
+        'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware' : None
 }
 
 # Configure item pipelines
